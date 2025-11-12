5881d7cbb4223c818e3f158beeb3b66f9173de13
XEXModz <tsbarnes07@gmail.com>
2025-11-12 03:31:28 -0500

    Change audio chunk size for playback

--- YB1.lua
+++ YB1.lua
@@ -2,7 +2,7 @@ local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 
 local decoder = dfpwm.make_decoder()
-for chunk in io.lines("1/Youngboy.dfpwm", 16*1024) do
+for chunk in io.lines("1/Youngboy.dfpwm", 5*236) do
   local buffer = decoder(chunk)
   while not speaker.playAudio(buffer) do
     os.pullEvent("speaker_audio_empty")
